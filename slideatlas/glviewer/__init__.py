8b4b047aaf9a96fd4f8148b18b59882865076b9d
Charles Law <charles.law@kitware.com>
2013-02-05 18:43:55 -0500

    fixed problem with backward compatability

--- slideatlas/glviewer/__init__.py
+++ slideatlas/glviewer/__init__.py
@@ -9,18 +9,18 @@ mod = Blueprint('glviewer', __name__,
                 url_prefix="/webgl-viewer"
                 )
 
-@mod.route('/single')
+@mod.route('')
 def glview_single():
     """
     - /glview?imgid=10239094124  searches for the session id
     """
 
     # See if the user is requesting any session id
-    viewid = request.args.get('view', None)
+    imgid = request.args.get('img', None)
     db = request.args.get('db', None)
 
-    if not viewid:
-        viewid = '5074528302e3100db8429cb4'
+    if not imgid:
+        imgid = '4f2808554834a30ccc000001'
 
     if not db:
         db = '5074589002e31023d4292d83'
@@ -28,20 +28,20 @@ def glview_single():
     conn.register([model.Database])
     admindb = conn["slideatlasv2"]
     dbobj = admindb["databases"].find_one({"_id" : ObjectId(db)})
-    viewdb = conn[dbobj['dbname']]
-
-    viewcollection = viewdb["views"]
-    myview = viewcollection.find_one({'_id':ObjectId(viewid)})
-    imagecollection = viewdb["images"]
-    myimage = imagecollection.find_one({'_id':myview["img"]})
+    imgdb = conn[dbobj['dbname']]
+    imgcollection = imgdb["images"]
+    myimage = imgcollection.find_one({'_id':ObjectId(imgid)})
     
     img = {}
     img["collection"] = str(myimage["_id"])
     img["origin"] = str(myimage["origin"])
     img["spacing"] = str(myimage["spacing"])
 
-    return render_template('myviewer.html', img = img, db = db)
-
+    return render_template('viewer.html', img = img, db = db)
+    
+    
+    
+    
 @mod.route('/dual')
 def glview_dual():
     """
@@ -71,7 +71,7 @@ def glview_dual():
     myquestion.databases = question.imgdbs
     myquestion.correct = -1
     
-    for index, viewid in enumerate(question.viewids)
-      myquestion.images.append(conn[question.viewdbs[index]]["images"].find_one({'_id':viewid}))
+    #for index, viewid in enumerate(question.viewids)
+    #  myquestion.images.append(conn[question.viewdbs[index]]["images"].find_one({'_id':viewid}))
 
     return render_template('mydualviewer.html', question=myquestion)
